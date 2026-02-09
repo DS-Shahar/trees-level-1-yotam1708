79013c68a4dc96dbc520bc5b070f98cacfd3bdb4
yotam1708 <yotam1708@gmail.com>
2026-02-09 14:18:51 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -53,3 +53,96 @@ public class RangeTree {
 }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import java.util.*;
+public class Main {
+    
+    public static boolean tree(BinNode<Integer> tree1) {
+        boolean bool = false ;
+        if (tree1 == null) {
+            return true;
+        }
+        if (tree1.getRight()!=null){
+         if (tree1.getLeft()!=null){
+             bool = true;
+         }  
+        }
+        if (tree1.getLeft()!=null){
+             bool = true;
+         } 
+        return tree(tree1.getLeft()) && tree(tree1.getRight());
+    }
+}
+
+
+
+
+
+import java.util.*;
+public class Main {
+
+class Range {
+    int low;
+    int high;
+
+    public Range(int low, int high) {
+        this.low = low;
+        this.high = high;
+    }
+}
+
+class Node {
+    Range value;
+    Node left;
+    Node right;
+
+    public Node(Range value) {
+        this.value = value;
+    }
+}
+
+public class RangeTree {
+
+    public static boolean order(Node t) {
+        if (t == null){
+            return true;
+        }
+        Range c = t.value;
+
+        if (t.left != null) {
+            Range left = t.left.value;
+
+            if (left.low != c.low || left.high > c.high)
+                return false;
+        }
+
+        if (t.right != null) {
+            Range right = t.right.value;
+
+            if (right.high != c.high || right.low < c.low)
+                return false;
+        }
+
+        if (t.left != null && t.right != null) {
+            if (t.left.value.high >= t.right.value.low)
+                return false;
+        }
+
+        return order(t.left) && order(t.right);
+    }
+}
+}
+
